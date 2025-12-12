56864c711a41293752de3cad27becd96f8ffd7e8
nauravlrie <nauravlrie@gmail.com>
2025-12-12 07:17:12 +0700

    m8

--- CM2Magang11.java
+++ CM2Magang11.java
@@ -1,13 +1,13 @@
 import java.util.Scanner; //JESYCA ALICE SUTANTO SIB 1A NO 11
 
 public class CM2Magang11 {
-    static String [][] dataMagang = new String [100][6];
+    static String [][] dataMagang = new String [100][6]; //nama, nim, prodi dll
     static int jumlahData = 0;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pilihan;
        
-        do {
+        do { //menggunakan loop do while sampai pengguna memilih keluar
             System.out.println("\n=== Sistem Pendaftaran Magang Mahasiswa ===");
             System.out.println("1. Tambah Data Magang Mahasiswa");
             System.out.println("2. Tampilkan Semua Pendaftar Magang");
@@ -17,7 +17,7 @@ public class CM2Magang11 {
             System.out.print("Pilih menu (1-5): ");
            
             pilihan = input.nextInt();
-            input.nextLine();
+            //if else untuk piihan
             if (pilihan == 1) tambahData(input);
             else if (pilihan == 2) tampilkanData();
             else if (pilihan == 3) cariData(input);
@@ -26,14 +26,14 @@ public class CM2Magang11 {
             else System.out.println("Pilihan tidak valid!");
         } while (pilihan != 5); //kondisi untuk terus mengulang jika pilihan diluar 5, karena kalau 5 sudah berhenti dan tidak akan mengulangi pertanyaan lgi
             //kalau enter 5 maka keluar loop
-        input.close();
+        input.close(); //tutup scanner
     }
 
-    //menambah data dan validasi semester dan status
+    //menambah data magang dan memvalidasi agar jumlah tidak melebihi 100
     static void tambahData(Scanner input) {
         if (jumlahData >= 100) {
             System.out.println("Data sudah penuh!");
-            return;
+            return; //keluar dari pengecekan
         }
 
         System.out.println("\n--- Tambah Data Magang ---");
@@ -55,6 +55,8 @@ public class CM2Magang11 {
             if (semester.equals("6") == false && semester.equals("7") == false)
                 System.out.println("Hanya boleh 6 atau 7!"); 
         } while (semester.equals("6") == false && semester.equals("7") == false); //mengulangi loop jika input bukan 6 atau 7
+        
+        
         String status;
         do {
             System.out.print("Status magang (Diterima/Menunggu/Ditolak): ");
@@ -76,10 +78,11 @@ public class CM2Magang11 {
         dataMagang[jumlahData][4] = semester;
         dataMagang[jumlahData][5] = status;
         jumlahData++;
+
         System.out.println("Data pendaftaran magang berhasil ditambahkan. Total pendaftar: " + jumlahData);
     }
 
-    //menampilkan semua data
+    //menampilkan semua data pendaftar
     static void tampilkanData() {
         if (jumlahData == 0) {
             System.out.println("\nBelum ada data pendaftar.");
@@ -94,7 +97,7 @@ public class CM2Magang11 {
         for (int i = 0; i < jumlahData; i++) {
             System.out.printf("%-3d %-15s %-10s %-20s %-15s %-8s %-10s\n",
                     (i+1), dataMagang[i][0], dataMagang[i][1], dataMagang[i][2],
-                    dataMagang[i][3], dataMagang[i][4], dataMagang[i][5]);
+                    dataMagang[i][3], dataMagang[i][4], dataMagang[i][5]); //mencetak data lanjutan sesuai format
         }
 
         System.out.println("Total pendaftar: " + jumlahData);
@@ -105,13 +108,17 @@ public class CM2Magang11 {
         System.out.println("\n--- Cari Data Berdasarkan Program Studi ---");
         System.out.print("Masukkan Program Studi: ");
         String cari = input.nextLine();
+
         System.out.println("Team Teaching Dasar Pemrograman 2025 Politeknik Negeri Malang");
         System.out.printf("%-3s %-15s %-10s %-20s %-15s %-8s %-10s\n",
                 "No", "Nama", "NIM", "Prodi", "Perusahaan", "Semester", "Status");
         System.out.println("--------------------------------------------------------------------------------");
-        boolean ketemu = false;
+        
+        boolean ketemu = false; //deklarasi 
+        //loop untuk setiap data
         for (int i = 0; i < jumlahData; i++) {
-            if (dataMagang[i][2].contains(cari)) {
+            if (dataMagang[i][2].contains(cari)) { //indeks 2/ kolom ketiga,berarti mencari dalam prodi
+
                 System.out.printf("%-3d %-15s %-10s %-20s %-15s %-8s %-10s\n",
                         (i+1), dataMagang[i][0], dataMagang[i][1], dataMagang[i][2],
                         dataMagang[i][3], dataMagang[i][4], dataMagang[i][5]);
@@ -120,16 +127,18 @@ public class CM2Magang11 {
         }
         if (ketemu == false) System.out.println("Tidak ditemukan data.");
     }
-    
+
     //menghitung jumlah per status
     static void hitungStatus() {
         if (jumlahData == 0) {
             System.out.println(" \nBelum ada pendaftar.");
             return;
         }
+
         int diterima = 0, menunggu = 0, ditolak = 0;
+
         for (int i = 0; i < jumlahData; i++) {
-            String s = dataMagang[i][5];
+            String s = dataMagang[i][5]; //mengambil status
             if (s.equalsIgnoreCase("diterima")) diterima++;
             else if (s.equalsIgnoreCase("menunggu")) menunggu++;
             else if (s.equalsIgnoreCase("ditolak")) ditolak++;
